4d6bd0139971d05384efa0677f66e597829b94b0
Akshai Raj <akshai293@gmail.com>
2025-12-12 21:56:05 +0530

    remove least recently used

--- lru.py
+++ lru.py
@@ -6,5 +6,16 @@ def access_page(page):
         cache.remove(page)   # remove old position
         cache.append(page)   # move to most recently used
         print(f"Page {page} accessed → HIT")
-    
-    
+    else:
+        print(f"Page {page} accessed → MISS")
+        if len(cache) == cache_size:
+            removed = cache.pop(0)   # remove least recently used
+            print("Removed LRU page:", removed)
+        cache.append(page)
+
+# Example access sequence
+pages = [1, 2, 3, 1, 4, 5]
+
+for p in pages:
+    access_page(p)
+    print("Cache:", cache)
